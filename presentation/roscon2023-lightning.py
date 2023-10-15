a1349a1e978db1d90d1083ed7d12db1605ffe62d
Griswald Brooks <griswald.brooks@gmail.com>
2023-10-15 17:47:35 +0000

    First roscon version

---
+++ presentation/roscon2023-lightning.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python3
+import elsie
+from elsie.ext import unordered_list
+from elsie.boxtree.box import Box
+
+slides = elsie.SlideDeck(width=1920, height=1080)
+
+slides.update_style("default", elsie.TextStyle(font="Lato", align="left", size=64))
+slides.update_style("code", elsie.TextStyle(size=38))
+slides.set_style("link", elsie.TextStyle(color="blue"))
+
+
+def logo_header_slide(parent: Box, title: str):
+    # parent.box(x=1570, y=40).image("picknik_logo.png")
+    parent.sbox(name="header", x=0, height=140).fbox(p_left=20).text(
+        title, elsie.TextStyle(bold=True)
+    )
+    return parent.fbox(name="content", p_left=20, p_right=20)
+
+
+def image_slide(parent: Box, title: str, image_path: str):
+    content = logo_header_slide(parent, title)
+    content = content.fbox(horizontal=True, p_top=20, p_bottom=20)
+    text_area = content.fbox(name="text_area", width="50%")
+    content.sbox(name="image", width="fill").image(image_path)
+    return text_area
+
+
+def section_title_slide(parent: Box, title: str, subtitle: str):
+    content = logo_header_slide(parent, "")
+    content.sbox().text(title, elsie.TextStyle(align="right", size=240, bold=True))
+    content.box().text(subtitle)
+
+
+def code_slide(parent: Box, title: str, language: str, code: str):
+    content = logo_header_slide(parent, title)
+    code_bg = "#F6F8FA"
+    box = content.box(y=0, width="100%", height="100%", p_bottom=20)
+    box.rect(bg_color=code_bg, rx=20, ry=20)
+    box.overlay().box(x=0, y=0, p_left=20, p_right=20, p_top=20, p_bottom=20).code(
+        language, code
+    )
+
+@slides.slide(debug_boxes=False)
+def normal_usage(slide):
+    code_slide(
+        slide,
+        "spanny: mdspan",
+        "C++",
+        """
+// Allocate the map
+std::array map_storage{...};
+// Create a 2D view of the map
+auto occupancy_map = std::mdspan(map_storage.data(), 384, 384);
+// Update the map 
+for (std::size_t i = 0; i != occupancy_map.extent(0); i++) {
+    for (std::size_t j = 0; j != occupancy_map.extent(1); j++) {
+        occupancy_map[i, j] = get_occupancy_from_laser_scan(...)
+    }
+}
+""",
+    )
+    slide.overlay().box(x=1200, y=570).image("images/turtlebot3_world.jpg", scale=1.0)
+    slide.set_style("footer", elsie.TextStyle(color="black", size=32, align="right"))
+    footer = slide.box(width="fill", height="5%", horizontal=True)
+    f1 = footer.box(width="fill", height="fill")
+    f1.text("Griswald Brooks   |   github.com/griswaldbrooks/spanny", "footer")
+
+
+@slides.slide(debug_boxes=False)
+def policy_injection(slide):
+    code_slide(
+        slide,
+        "spanny: policy injection",
+        "C++",
+        """
+using bin_view = 
+std::mdspan<class T, class Extents, class LayoutPolicy, class Accessor>;
+""",
+    )
+    slide.set_style("footer", elsie.TextStyle(color="black", size=32, align="right"))
+    footer = slide.box(width="fill", height="5%", horizontal=True)
+    f1 = footer.box(width="fill", height="fill")
+    f1.text("Griswald Brooks   |   github.com/griswaldbrooks/spanny", "footer")
+
+
+@slides.slide(debug_boxes=False)
+def async_bin_checker(slide):
+    code_slide(
+        slide,
+        "spanny: bin accessor",
+        "C++",
+        """
+using bin_view = 
+std::mdspan<class T, class Extents, class LayoutPolicy, bin_checker>;
+
+struct bin_checker {
+  using element_type = bin_state;
+  using reference = std::future<bin_state>;
+  using data_handle_type = robot_arm*;
+
+  reference access(data_handle_type ptr, std::ptrdiff_t offset) const {
+    return std::async([=]{
+      return bin_state{ptr->is_bin_occupied(static_cast<int>(offset)),
+                       offset_to_coord(static_cast<int>(offset))};
+    });
+  }
+};
+""",
+    )
+    slide.set_style("footer", elsie.TextStyle(color="black", size=32, align="right"))
+    footer = slide.box(width="fill", height="5%", horizontal=True)
+    f1 = footer.box(width="fill", height="fill")
+    f1.text("Griswald Brooks   |   github.com/griswaldbrooks/spanny", "footer")
+
+@slides.slide(debug_boxes=False)
+def bounds_checker(slide):
+    code_slide(
+        slide,
+        "spanny: bin layout",
+        "C++",
+        """
+using bin_view = 
+std::mdspan<class T, class Extents, bin_layout, bin_checker>;
+
+struct bin_layout {
+  template <class Extents>
+  struct mapping : stdex::layout_right::mapping<Extents> {
+    using base_t = stdex::layout_right::mapping<Extents>;
+    using base_t::base_t;
+    std::ptrdiff_t operator()(auto... idxs) const {
+      [&]<size_t... Is>(std::index_sequence<Is...>) {
+        if (((idxs < 0 || idxs > this->extents().extent(Is)) || ...)) {
+          throw std::out_of_range("Invalid bin index");
+        }
+      }(std::make_index_sequence<sizeof...(idxs)>{});
+      return this->base_t::operator()(idxs...);
+    }
+  };
+};
+""",
+    )
+    slide.set_style("footer", elsie.TextStyle(color="black", size=32, align="right"))
+    footer = slide.box(width="fill", height="5%", horizontal=True)
+    f1 = footer.box(width="fill", height="fill")
+    f1.text("Griswald Brooks   |   github.com/griswaldbrooks/spanny", "footer")
+
+
+@slides.slide(debug_boxes=False)
+def async_implementation(slide):
+    code_slide(
+        slide,
+        "spanny: beer view",
+        "C++",
+        """
+using bin_view = 
+std::mdspan<bin_state, six_pack, bin_layout, bin_checker>;
+
+auto arm = robot_arm{"/dev/ttyACM0", 9600};
+auto bins = bin_view(&arm);
+while(true) {
+  std::vector<std::future<bin_state>> futures;
+  for (auto i = 0u; i < bins.extent(0); ++i) 
+    for (auto j = 0u; j < bins.extent(1); ++j) 
+        futures.push_back(bins(i, j));
+    
+  for (auto const& future : futures) future.wait();
+    
+  for (auto& future : futures) print_state(future.get());
+    
+  std::cout << "====================" << std::endl;
+}
+""",
+    )
+    slide.set_style("footer", elsie.TextStyle(color="black", size=32, align="right"))
+    footer = slide.box(width="fill", height="5%", horizontal=True)
+    f1 = footer.box(width="fill", height="fill")
+    f1.text("Griswald Brooks   |   github.com/griswaldbrooks/spanny", "footer")
+
+
+@slides.slide(debug_boxes=False)
+def demo_time(slide):
+    content = logo_header_slide(slide, "")
+    content.box(width="fill").text(
+        "Demo Time", elsie.TextStyle(size=160, bold=True)
+    )
+    
+    slide.set_style("footer", elsie.TextStyle(color="black", size=32, align="right"))
+    footer = slide.box(width="fill", height="5%", horizontal=True)
+    f1 = footer.box(width="fill", height="fill")
+    f1.text("Griswald Brooks   |   github.com/griswaldbrooks/spanny", "footer")
+    
+@slides.slide(debug_boxes=False)
+def thank_you(slide):
+    content = logo_header_slide(slide, "")
+    content.fbox().text(
+        "Thank you!\n",
+        elsie.TextStyle(align="middle", size=80, bold=True),
+    )
+    content.fbox().text(
+        "Special Thanks to\nDaisy Hollman and Tyler Weaver\n~link{github.com/griswaldbrooks/spanny}\n",
+        elsie.TextStyle(align="middle"),
+    )
+    content.sbox(p_bottom=20).text(
+        "Slides generated using Elsie\n",
+        elsie.TextStyle(align="right", size=32),
+    )
+
+
+slides.render("roscon2023-lightning.pdf")
